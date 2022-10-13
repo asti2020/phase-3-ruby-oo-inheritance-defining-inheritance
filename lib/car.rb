69513ad216db9a15b27beb0f0a5778b3b2b534ce
Astika Luintel <astild2020@gmail.com>
2022-10-13 19:24:34 -0400

    commit tree f1cdd5ddaa733d8845317d5b7546915d953f8162

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,9 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+class Car < Vehicle
+    def go
+        "form car ghrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
+    end
+
+end
+self.wheel_number = 2
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,16 @@
 class Vehicle
 
+    attr_accessor :wheel_size, :wheel_numbr
+    def initialize(wheel_size, wheel_number = 2)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+
+    def go
+        "ghrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr"
+    end
+
+    def start
+        "filling up"
+    end
 end
